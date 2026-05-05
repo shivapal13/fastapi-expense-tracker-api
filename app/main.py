7006ccb5891c75ac978b05da2c1fc54c908e7d75
Shiva Pal <spal13138@gmail.com>
2026-05-06 00:44:16 +0530

    change main.py

--- app/main.py
+++ app/main.py
@@ -6,8 +6,6 @@ from app import models
 from app.routers import expense
 from app.routers import user
 from app.routers import auth
-import os
-print("DATABASE_URL:", os.getenv("DATABASE_URL"))
 
 models.Base.metadata.create_all(bind=engine)
 
