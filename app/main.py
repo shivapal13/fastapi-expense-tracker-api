5f03cf45a7f04c97f941b354b4b66a2915a86594
Shiva Pal <spal13138@gmail.com>
2026-05-06 01:44:46 +0530

    fix deploy issues error

--- app/main.py
+++ app/main.py
@@ -1,29 +1,18 @@
-from dotenv import load_dotenv
-load_dotenv()
 import traceback
 
 try:
-    print("APP STARTING...")
-except Exception:
-    traceback.print_exc()
+    from fastapi import FastAPI
+    from app.routers import user, auth, expense
 
-try:
-    from fastapi import FastAPI 
-    from app import models
-    from app.routers import expense, user, auth
     print("IMPORTS OK")
+
+    app = FastAPI()
+
+    app.include_router(user.router)
+    app.include_router(auth.router)
+    app.include_router(expense.router)
+
+    print("APP READY")
+
 except Exception:
     traceback.print_exc()
-
-app=FastAPI()
-
-#models.Base.metadata.create_all(bind=engine)
-
-
-
-
-app.include_router(expense.router)
-app.include_router(user.router)
-app.include_router(auth.router)
-
-
