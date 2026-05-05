137ecfe1638ede15411843af3ab45d9b2a27206d
Shiva Pal <spal13138@gmail.com>
2026-05-06 00:40:20 +0530

    debug env variables

--- app/database.py
+++ app/database.py
@@ -1,15 +1,19 @@
+from dotenv import load_dotenv
+load_dotenv()
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
-from app.config import settings
 import os
 
-SQLALCHEMY_DATABASE_URL=os.getenv("SQLALCHEMY_DATABASE_URL")
+SQLALCHEMY_DATABASE_URL=os.getenv("DATABASE_URL")
 
-engine=create_engine(
-    SQLALCHEMY_DATABASE_URL,
-    connect_args={"sslmode":"require"}
-)
+if "localhost" in SQLALCHEMY_DATABASE_URL:
+    engine = create_engine(SQLALCHEMY_DATABASE_URL)
+else:
+    engine = create_engine(
+        SQLALCHEMY_DATABASE_URL,
+        connect_args={"sslmode": "require"}
+    )
 
 SessionLocal=sessionmaker(autocommit=False,autoflush=False,bind=engine)
 
--- app/main.py
+++ app/main.py
@@ -1,3 +1,5 @@
+from dotenv import load_dotenv
+load_dotenv()
 from fastapi import FastAPI
 from app.database import Base,engine
 from app import models
