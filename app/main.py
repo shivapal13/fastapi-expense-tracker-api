21a197793414cf4775e86fc3c0acc4fb53485137
Shiva Pal <spal13138@gmail.com>
2026-05-07 20:12:11 +0530

    modify main file

--- app/main.py
+++ app/main.py
@@ -1,18 +1,8 @@
-import traceback
+from fastapi import FastAPI
+from app.routers import user, auth, expense
 
-try:
-    from fastapi import FastAPI
-    from app.routers import user, auth, expense
+app = FastAPI()
 
-    print("IMPORTS OK")
-
-    app = FastAPI()
-
-    app.include_router(user.router)
-    app.include_router(auth.router)
-    app.include_router(expense.router)
-
-    print("APP READY")
-
-except Exception:
-    traceback.print_exc()
+app.include_router(user.router)
+app.include_router(auth.router)
+app.include_router(expense.router)
