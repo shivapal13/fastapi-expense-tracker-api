d4eaebffeefe68b63442b9f8b887cd4445457c5a
Shiva Pal <spal13138@gmail.com>
2026-05-07 20:40:05 +0530

    create tables in production db

--- app/main.py
+++ app/main.py
@@ -1,6 +1,9 @@
 from fastapi import FastAPI
 from app.routers import user, auth, expense
+from app import models
+from app.database import engine
 
+models.Base.metadata.create_all(bind=engine)
 app = FastAPI()
 
 app.include_router(user.router)
