bb640790e0019a1d196093b8e6f934c8c6e912c0
Shiva Pal <spal13138@gmail.com>
2026-05-08 00:04:29 +0530

    modified database.py file

--- app/database.py
+++ app/database.py
@@ -1,27 +1,17 @@
-from dotenv import load_dotenv
-load_dotenv()
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy.ext.declarative import declarative_base
 import os
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
 
-SQLALCHEMY_DATABASE_URL=os.getenv("DATABASE_URL")
+DATABASE_URL = os.getenv("DATABASE_URL")
 
-if "localhost" in SQLALCHEMY_DATABASE_URL:
-    engine = create_engine(SQLALCHEMY_DATABASE_URL)
-else:
-    engine = create_engine(
-        SQLALCHEMY_DATABASE_URL,
-        connect_args={"sslmode": "require"}
-    )
+if not DATABASE_URL:
+    raise ValueError("DATABASE_URL is missing")
 
-SessionLocal=sessionmaker(autocommit=False,autoflush=False,bind=engine)
+# Always use SSL on Render
+engine = create_engine(
+    DATABASE_URL,
+    connect_args={"sslmode": "require"}
+)
 
-Base=declarative_base()
-
-def get_db():
-    db=SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()    
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base()
